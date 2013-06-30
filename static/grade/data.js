84399ddaee4a9f653b0c77dcf3fd67ba572f1c6e
marcio <taboca@gmail.com>
2013-06-30 15:43:06 -0300

    Rearranging custom settings to data

--- static/grade/data.js
+++ static/grade/data.js
@@ -1,3 +1,43 @@
+var util_roomNameReplacer = new Array(); 
+util_roomNameReplacer[8]='40T';
+util_roomNameReplacer[1]='41A';
+util_roomNameReplacer[2]='41B';
+util_roomNameReplacer[3]='41C';
+util_roomNameReplacer[4]='41D';
+util_roomNameReplacer[5]='41E';
+util_roomNameReplacer[6]='41F';
+util_roomNameReplacer[7]='40A';
+util_roomNameReplacer[716]='P09';
+util_roomNameReplacer[11]='P11';
+util_roomNameReplacer[701]='701';
+util_roomNameReplacer[702]='702';
+util_roomNameReplacer[715]='710';
+util_roomNameReplacer[723]='713';
+util_roomNameReplacer[714]='714';
+util_roomNameReplacer[722]='715';
+util_roomNameReplacer[717]='F12';
+util_roomNameReplacer[718]='F13';
+
+var util_columnOrderReference = new Array();
+util_columnOrderReference[0]='40T';
+util_columnOrderReference[1]='41A';
+util_columnOrderReference[2]='41B';
+util_columnOrderReference[3]='41C';
+util_columnOrderReference[4]='41D';
+util_columnOrderReference[5]='41E';
+util_columnOrderReference[6]='41F';
+util_columnOrderReference[7]='40A';
+util_columnOrderReference[8]='P09';
+util_columnOrderReference[9]='P11';
+util_columnOrderReference[10]='701';
+util_columnOrderReference[11]='702';
+util_columnOrderReference[12]='710';
+util_columnOrderReference[13]='713';
+util_columnOrderReference[14]='714';
+util_columnOrderReference[15]='715';
+util_columnOrderReference[16]='F12';
+util_columnOrderReference[17]='F13';
+
 
 var evento = {
 
