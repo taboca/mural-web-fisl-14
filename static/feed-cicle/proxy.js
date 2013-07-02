b5316139b5d50f510c5b9c1bb02fa7b4d636ef17
marcio <taboca@gmail.com>
2013-07-02 17:22:47 -0300

    Identi.ca

--- static/feed-cicle/proxy.js
+++ static/feed-cicle/proxy.js
@@ -1,9 +1,11 @@
 
 doFilter = function (that) { 
  var title   = $(that).find('title').text();
+ var image   = $(that).find('statusnet\\:postIcon').attr('rdf:resource');
+ var link = $(this).find('link[rel="enclosure"]');
  var link = $(that).find('description').text();
  $('#temp').html(link);
  var plainDesc = $('#temp').text();	
- return {'title':title, 'who':'', 'description':plainDesc };
+ return {'title':title, 'src': image, 'description':plainDesc };
 }
 
