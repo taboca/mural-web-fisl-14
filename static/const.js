97e1ac5eaeb3045960cfba4a8e198f1ddd31f667
marcio <taboca@gmail.com>
2014-05-07 01:36:46 -0300

    FISL15

--- static/const.js
+++ static/const.js
@@ -1,4 +1,4 @@
 URL_FEED = '/channel/identica.xml'
 URL_FOTOS = '/channel/instagram-fisl15.json'
-URL_TWIT = "/channel/fisl.txt";
+URL_TWIT = "/channel/fisl15.txt";
 URL_DESTAQUES = "/channel/destaques.xml";
