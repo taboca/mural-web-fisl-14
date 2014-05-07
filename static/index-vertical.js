2db76031323be2a36f05e33c526c674a80593649
marcio <taboca@gmail.com>
2014-05-07 01:33:06 -0300

    Twitter is back

--- static/index-vertical.js
+++ static/index-vertical.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
    register("/main/middle", "meio", "./grade-vertical/index.html", iframeTemplate);
    register("/main/topheader", "topo", "./header-vertical/index.html", iframeTemplate);
    register("/main/topmiddle", "mid", "./header-vertical/index.html", iframeTemplate);
-   register("/main/bottomsection2", "tweeter", "http://www.telasocial.com:8888/static/twitter-new/index.html", iframeTemplate);
+   register("/main/bottomsection2", "tweeter", "./twitter-new/index.html", iframeTemplate);
    register("/main/bottomsection", "identica", "./feed-cicle/index.html", iframeTemplate);
    register("/main/bottomsection-b", "fotos", "./galeria/index.html", iframeTemplate);
 
